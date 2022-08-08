443164b4ad94239c8f1c6147dd94305c93125a76
JoseDiegoMarinContreras <DIEGOmai195@hotmail.com>
2022-08-08 17:02:35 -0600

    django server template

---
+++ django_server_template/greetings.py
@@ -0,0 +1,2 @@
+def greet(request):
+    "Hello human (^:"
--- django_server_template/settings.py
+++ django_server_template/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -48,6 +49,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+SERVER_PORT = getenv("PORT") if getenv("PORT") else ""
 
 ROOT_URLCONF = 'django_server_template.urls'
 
